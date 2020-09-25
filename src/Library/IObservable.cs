284c7feb30efb05978315099abc4bc0e4e879501
Nicolas <nicolas.lozano.282002@gmail.com>
2020-09-25 20:01:39 -0300

    .

---
+++ src/Library/IObservable.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Observer
+{
+    public interface IObservable
+    {
+        void Subscribe(IObserver observer);
+        void Unsubscribe(IObserver observer);
+    }
+}
